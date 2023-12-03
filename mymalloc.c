b83c04fd9f771b6130f3815f8ea5d32fb458bae2
David <rajlic.david@gmail.com>
2023-12-03 17:56:06 +0100

    can store multiple segments in same page, all tests working except 6 and 8

--- mymalloc.c
+++ mymalloc.c
@@ -1,88 +1,147 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <sys/mman.h>
 
-#define PAGE_SIZE 4096
-
-struct alok_info {
-   size_t velikost_alokacije; // velikost alokacije uporabljena pri mmap
-   size_t stevilo_odsekov;    // trenutno število odsekov
-   void *prazen_odsek;        // kazalec na prazen odsek
-   void *next;
+struct alok_info
+{
+    size_t velikost_strani;
+    size_t stevilo_odsekov;
+    struct alok_info *nova_stran; //naslednji prost
+    struct odsek_info *firstOdsek;
 };
 
-struct odsek_info {
-   size_t velikost_odseka; // velikost odseka
-   void *stran;            // kazalec na začetek alocirane strani
+struct odsek_info
+{
+    size_t velikost_odseka;
+    struct alok_info *alok_info;
+    struct odsek_info *next;
 };
 
-int st_strani = 0;
-int st_odsekov = 0;
-void *zadnji_naslov = NULL;
-void *zacetek_naslov = NULL;
+struct alok_info *glava = NULL;
 
-struct  alok_info *first = NULL;
-struct  alok_info *current = NULL;
-
-void *mymalloc(size_t size) {
-    if (size == 0) {
+void *mymalloc(size_t size)
+{
+    
+    if (size == 0)
+    {
         return NULL;
     }
 
-    int velikost_strani = getpagesize();
-
-    if (first == NULL || size > PAGE_SIZE) {
-        size_t velikost_alokacije = size + sizeof(struct alok_info) + sizeof(struct odsek_info);
+    //Prvo alociranje 
+    if (glava == NULL) 
+    {
+        size_t velikost_alokacije = size + sizeof(struct odsek_info) + sizeof(struct alok_info);
+        
+        size_t velikost_strani = getpagesize(); //4096 zlogov
 
         size_t ostanek = velikost_alokacije % velikost_strani;
-        if (ostanek > 0) {
+        if (ostanek > 0)
+        {
             velikost_alokacije += velikost_strani - ostanek;
         }
+        struct alok_info *stran = mmap(NULL, velikost_alokacije, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-        void *stran = mmap(NULL, velikost_alokacije, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        if (stran == MAP_FAILED) {
+        if (stran == MAP_FAILED) //Če je napaka pri alokaciji
+        {
             return NULL;
         }
-        st_strani++;
-        st_odsekov++;
-
-        struct alok_info *new_alok_info = (struct alok_info *)stran;
-        new_alok_info->velikost_alokacije = velikost_alokacije;
-        new_alok_info->stevilo_odsekov = st_odsekov;
-        new_alok_info->prazen_odsek = stran + sizeof(struct odsek_info);
-        zadnji_naslov = new_alok_info->prazen_odsek;
-        current = new_alok_info;
-        return new_alok_info->prazen_odsek;
-    } else {
-        struct odsek_info *odsek_info = zadnji_naslov;
-        odsek_info->stran = zadnji_naslov + size + sizeof(struct odsek_info);
-        zadnji_naslov = odsek_info->stran;
-        return zadnji_naslov;
+        //Nastavi vrednosti alokacije
+        stran->velikost_strani = velikost_alokacije;
+        stran->firstOdsek = (struct odsek_info *)(stran + 1);
+        stran->firstOdsek->alok_info = stran;
+        stran->firstOdsek->next = NULL;
+        stran->firstOdsek->velikost_odseka = size;
+        glava = (struct alok_info *)stran;
+        stran->stevilo_odsekov = 1;
+        return (char *)(glava) + sizeof(struct alok_info) + sizeof(struct odsek_info);
     }
+
+    struct alok_info *currentStran = glava;
+    bool search = true;
+
+    while (search) // išči stran
+    {
+        struct odsek_info *currentOdsek = currentStran->firstOdsek;
+        while (currentOdsek->next != NULL) // gremo do zadnjega odseka
+        {
+            currentOdsek = currentOdsek->next;
+        }
+        size_t zapolnjeno = ((char *)currentOdsek + currentOdsek->velikost_odseka - (char*) currentStran); // izračun zapolnjnenga dela strani 
+       
+        if (currentStran->velikost_strani - zapolnjeno > size) //ali je še dovolj prostora v tej strani
+        {
+            if (currentOdsek->next == NULL)
+            {
+                currentOdsek->next = (struct odsek_info *)((char *)(currentOdsek + 1) + size);
+                currentOdsek->next->alok_info = glava;
+                currentOdsek->next->velikost_odseka = size;
+                currentOdsek->next->next = NULL;
+                glava->stevilo_odsekov++;
+                return (void *)(currentOdsek->next + 1);
+            }
+        }
+        else // Če ni dovolj prostora bomo ustvarili novo stran
+        {
+            size_t velikost_alokacije = size + sizeof(struct odsek_info) + sizeof(struct alok_info);
+            size_t velikost_strani = getpagesize();
+
+            size_t ostanek = velikost_alokacije % velikost_strani;
+            if (ostanek > 0)
+            {
+                velikost_alokacije += velikost_strani - ostanek;
+            }
+
+            struct alok_info *stran = mmap(NULL, velikost_alokacije, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+            if (stran == MAP_FAILED)
+            {
+                return NULL;
+            }
+            stran->velikost_strani = velikost_alokacije;
+            
+            stran->firstOdsek = (struct odsek_info *)(stran + 1);
+            
+            stran->firstOdsek->alok_info = stran;
+            stran->firstOdsek->next = NULL;
+            stran->firstOdsek->velikost_odseka = size;
+            stran->stevilo_odsekov = 1;
+            currentStran->nova_stran = (struct alok_info *)stran;
+            return (char *)(currentStran->nova_stran) + sizeof(struct alok_info) + sizeof(struct odsek_info);
+        }
+    }
+    return NULL;
 }
 
-void myfree(void *ptr) {
-    if (ptr == NULL) {
+void myfree(void *ptr)
+{
+    if (ptr == NULL)
+    {
         return;
     }
 
-    // Koda za sproščanje pomnilnika
-    while (st_strani > 0) {
-        if (munmap(current, current->velikost_alokacije) == -1) {
+    struct odsek_info *odsek = ((struct odsek_info *)ptr) - 1; 
+    struct alok_info *stran = odsek->alok_info;
+    if (stran == glava)
+    {
+        if (glava->nova_stran == NULL) {
+             glava = NULL; 
+        }   
+        else {
+            glava = glava->nova_stran;
+        }
+           
+    }
+  
+    stran->stevilo_odsekov--;
+    if (stran->stevilo_odsekov == 0)
+    {
+        if (munmap(stran, stran->velikost_strani) == -1)
+        {
             perror("Error in munmap");
-        } else {
-            st_strani--;
         }
     }
-    first = NULL;
 }
-
-
-
-
-// gcc main.c -o main.o -c
-// gcc mymalloc.c -o mymalloc.o -c
-// gcc -o main main.o mymalloc.o
